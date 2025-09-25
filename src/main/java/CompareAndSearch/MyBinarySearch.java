00908638e1fdf455d9d94b12935d3a227cbe2095
_AlexSf_ <minor83@mail.ru>
2025-09-25 16:51:05 +0300

    BinarySearch Added

---
+++ src/main/java/CompareAndSearch/MyBinarySearch.java
@@ -0,0 +1,39 @@
+package CompareAndSearch;
+
+import java.util.List;
+
+public class MyBinarySearch<T>{
+    private final List<? extends Comparable<? super T>> list;
+    private final T search;
+    private final int indexedBinarySearch;
+
+    public MyBinarySearch(List<? extends Comparable<? super T>> list, T search) {
+        this.list=list;
+        this.search=search;
+        this.indexedBinarySearch=indexedBinarySearch();
+    }
+
+    public int getIndexedBinarySearch() {
+        return indexedBinarySearch;
+    }
+
+    private int indexedBinarySearch() {
+        int low = 0;
+        int high = list.size()-1;
+
+        while (low <= high) {
+            int middle = (low + high) >>> 1;
+            Comparable<? super T> midValue = list.get(middle);
+            int tmp = midValue.compareTo(search);
+
+            if (tmp < 0)
+                low = middle + 1;
+            else if (tmp > 0)
+                high = middle - 1;
+            else
+                return middle; // found
+        }
+        return -(low + 1);  //not found
+    }
+}
+
